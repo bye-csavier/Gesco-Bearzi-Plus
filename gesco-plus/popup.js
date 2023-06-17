62b99be097812ce502884ac309fffde179138497
bye-csavier <xavidp05@gmail.com>
2023-06-18 00:45:19 +0200

    Aggiornato popup
    Ora nel popup dell'estensione si trovano sia un bottone che porta alla nostra pagina principale sia un link che rimanda al github

--- gesco-plus/popup.js
+++ gesco-plus/popup.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    var optionsButton = document.getElementById('openOptions');
+    optionsButton.addEventListener('click', openOptions);
 
-document.addEventListener("DOMContentLoaded", () => {});
+});
+
+function openOptions() {
+    if (chrome.runtime.openOptionsPage) 
+    {
+        chrome.runtime.openOptionsPage();
+    } else {
+        window.open(chrome.runtime.getURL('options.html'));
+    }
+}
